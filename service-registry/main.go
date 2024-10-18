4753c9251fe41353340e69ac3bad850dd71dc648
shunlog <artiombalan331@gmail.com>
2024-10-18 20:12:33 +0300

    added protobuf & generated gRPC code

--- service-registry/main.go
+++ service-registry/main.go
@@ -1,41 +1,48 @@
 package main
 
 import (
-	"net/http"
-	"os"
+    "context"
+    "log"
+    "net"
+    "sync"
 
-	"github.com/labstack/echo/v4"
-	"github.com/labstack/echo/v4/middleware"
+    "google.golang.org/grpc"
+    pb "example/docker-ping/registry"
 )
 
-func main() {
-
-	e := echo.New()
-
-	e.Use(middleware.Logger())
-	e.Use(middleware.Recover())
-
-	e.GET("/", func(c echo.Context) error {
-		return c.HTML(http.StatusOK, "Hello, Docker! <3")
-	})
-
-	e.GET("/health", func(c echo.Context) error {
-		return c.JSON(http.StatusOK, struct{ Status string }{Status: "OK"})
-	})
-
-	httpPort := os.Getenv("PORT")
-	if httpPort == "" {
-		httpPort = "8080"
-	}
-
-	e.Logger.Fatal(e.Start(":" + httpPort))
+type registryServer struct {
+    pb.UnimplementedServiceRegistryServer
+    services map[string][]*pb.ServiceInfo
+    mu       sync.Mutex
 }
 
-// Simple implementation of an integer minimum
-// Adapted from: https://gobyexample.com/testing-and-benchmarking
-func IntMin(a, b int) int {
-	if a < b {
-		return a
-	}
-	return b
+func (s *registryServer) RegisterService(ctx context.Context, info *pb.ServiceInfo) (*pb.RegisterResponse, error) {
+    s.mu.Lock()
+    defer s.mu.Unlock()
+
+    s.services[info.ServiceName] = append(s.services[info.ServiceName], info)
+    return &pb.RegisterResponse{Success: true}, nil
+}
+
+func (s *registryServer) GetServiceInstances(ctx context.Context, query *pb.ServiceQuery) (*pb.ServiceInstances, error) {
+    s.mu.Lock()
+    defer s.mu.Unlock()
+
+    instances := s.services[query.ServiceName]
+    return &pb.ServiceInstances{Instances: instances}, nil
+}
+
+func main() {
+    lis, err := net.Listen("tcp", ":50051")
+    if err != nil {
+        log.Fatalf("failed to listen: %v", err)
+    }
+
+    grpcServer := grpc.NewServer()
+    pb.RegisterServiceRegistryServer(grpcServer, &registryServer{services: make(map[string][]*pb.ServiceInfo)})
+
+    log.Println("Service Registry running on port 50051")
+    if err := grpcServer.Serve(lis); err != nil {
+        log.Fatalf("failed to serve: %v", err)
+    }
 }
