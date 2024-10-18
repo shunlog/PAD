36325a60e482fbe193eae8afb29575a9faf1645f
shunlog <artiombalan331@gmail.com>
2024-10-18 14:08:38 +0300

    use quart for websockets

---
+++ chat/app.py
@@ -0,0 +1,31 @@
+import asyncio
+
+from broker import Broker
+
+from quart import Quart, render_template, websocket
+
+
+app = Quart(__name__)
+broker = Broker()
+
+
+@app.get("/")
+async def index():
+    return await render_template("index.html")
+
+
+async def _receive() -> None:
+    while True:
+        message = await websocket.receive()
+        await broker.publish(message)
+
+
+@app.websocket("/ws")
+async def ws() -> None:
+    try:
+        task = asyncio.ensure_future(_receive())
+        async for message in broker.subscribe():
+            await websocket.send(message)
+    finally:
+        task.cancel()
+        await task
--- lobby-server/app.py
+++ lobby-server/app.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python
 
 from flask import Flask
+from websockets.asyncio.client import connect
 
 app = Flask(__name__)
 
 
 @app.route("/")
-def hello_world():
+async def hello_world():
+
+    async with connect("ws://websocket-server:8000/ws") as ws:
+        await ws.send("hello uwu!")
+
     return "<p>Hello, World!</p>"
